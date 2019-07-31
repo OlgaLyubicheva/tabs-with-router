f838c672eb8a6f766336af44d04a86fbf9d503ae
Olga Lyubicheva <forjobslov@gmail.com>
2019-07-31 15:02:30 +0300

    Updates

---
+++ precache-manifest.f1a7f42139381973a05eddef5b741c65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bda9ce684f48e9683c6f1bbc50e06cc",
+    "url": "/tabs-with-router/index.html"
+  },
+  {
+    "revision": "4076f3a71e91293fbd59",
+    "url": "/tabs-with-router/static/css/main.4ba34287.chunk.css"
+  },
+  {
+    "revision": "55c2ba76912a22540146",
+    "url": "/tabs-with-router/static/js/2.63bf0fae.chunk.js"
+  },
+  {
+    "revision": "4076f3a71e91293fbd59",
+    "url": "/tabs-with-router/static/js/main.e3ee621c.chunk.js"
+  },
+  {
+    "revision": "3513456d47be6d2df32f",
+    "url": "/tabs-with-router/static/js/runtime~main.8e076b95.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_tabs-with-router/precache-manifest.e4fbd0cc21fe98726b4170a222bbde40.js"
+  "/tabs-with-router/precache-manifest.f1a7f42139381973a05eddef5b741c65.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_tabs-with-router/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tabs-with-router/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
